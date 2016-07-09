1abab578a1ebd998ea4c37297f8a25d3fb6a5af3
Manish Menaria <Itss.manish.menaria@gmail.com>
2016-07-09 13:21:55 +0530

    GCE Deployed to server :)

---
+++ app/src/main/java/Utils/AsyncTask_FetchJoke.java
@@ -0,0 +1,51 @@
+package Utils;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+import optimist.mechanic.hnmn3.build_it_bigger.backend.myApi.MyApi;
+
+/**
+ * Created by Manish Menaria on 09-Jul-16.
+ */
+public class AsyncTask_FetchJoke extends AsyncTask<Context, List<String>, List<String>> {
+    private MyApi myApiService = null;
+    private Context context;
+    public AsyncResponse delegate = null;
+
+    @Override
+    protected List<String> doInBackground(Context... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://build-it-bigger-1366.appspot.com/_ah/api/");
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0];
+
+
+        try {
+            return myApiService.gceJokes().execute().getJokes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<String> s = null;
+        s.add("No jokes available");
+        return s;
+    }
+
+    @Override
+    protected void onPostExecute(List<String> result) {
+        delegate.processFinish(result);
+    }
+}
+
+
