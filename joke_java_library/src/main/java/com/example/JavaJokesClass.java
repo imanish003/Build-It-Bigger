76d003ee5dd484b3c0e86afe864ef49abdacefaf
Manish Menaria <Itss.manish.menaria@gmail.com>
2016-07-11 11:45:43 +0530

    Added some real funny Jokes :D

--- joke_java_library/src/main/java/com/example/JavaJokesClass.java
+++ joke_java_library/src/main/java/com/example/JavaJokesClass.java
@@ -2,7 +2,17 @@ package com.example;
 
 public class JavaJokesClass {
 
-    final static String[] jokes = {"Bahoot funny joke ", "Ye wala us se bhi funny joke :D"};
+    final static String[] jokes = {"Can a kangaroo jump higher than a house? \n Of course, a house does not jump at all."
+            , "My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away."
+            , "My wife's cooking is so bad we usually pray after our food."
+            , "Why do women live on average two years longer? \n" +
+            "Because the time they spend parking doesn't count."
+            , "The 21st century: \n" +
+            "Deleting history is often more important than making it."
+            ,"I've decided to run a marathon for charity.\n" +
+            "I didn't want to do it at first, \n" +
+            "but apparently it's for blind and disabled kids so I think I've got a good chance of winning."
+    };
 
     public String[] getJokes() {
         return jokes;
